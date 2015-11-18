7b80b0ae0f412b5d501f91c91b83d483bca457a8
Dax <gerts.dax1@gmail.com>
2015-11-18 02:53:40 -0500

    began configuring home page, currently unusable

--- dbproject/dbproject/settings.py
+++ dbproject/dbproject/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webstore'
+	'django.contrib.sites',
+    'webstore',
+    'home',
 )
 
 MIDDLEWARE_CLASSES = (
--- dbproject/dbproject/urls.py
+++ dbproject/dbproject/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from dbproject.home import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dbproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^$','home.views.index'),
+    url(r'^webstore/', include('webstore.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
