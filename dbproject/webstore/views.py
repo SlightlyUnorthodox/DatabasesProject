c61c2f36fcfe20910bc18177d76596569aec6f33
Dax <gerts.dax1@gmail.com>
2015-11-18 14:24:31 -0500

    homepage structure confiured working (accepted urls '/', '/index', and '/admin')

--- dbproject/webstore/views.py
+++ dbproject/webstore/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, render_to_response
-from django.template import RequestContext
+#from django.shortcuts import render, render_to_response
+#from django.template import RequestContext
 
 def index(request):
 	return HttpResponse("Welcome to [name of webstore here]'s index.")
