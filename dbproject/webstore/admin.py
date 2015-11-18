5968d3a03f1db6ac57ac6f1a1f5cc9b490233473
Dax <gerts.dax1@gmail.com>
2015-11-18 01:55:49 -0500

    configured basic admin

--- dbproject/webstore/admin.py
+++ dbproject/webstore/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User
+from .models import Order
+from .models import Supplier
+from .models import Contains
+from .models import Product
+
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Supplier)
+admin.site.register(Contains)
+admin.site.register(Product)
