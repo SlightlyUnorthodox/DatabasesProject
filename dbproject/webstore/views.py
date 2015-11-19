368ce872048e04ff0dae65a1bfee620b87504de5
Dax <gerts.dax1@gmail.com>
2015-11-18 20:31:09 -0500

    added redirect after successful login

--- dbproject/webstore/views.py
+++ dbproject/webstore/views.py
@@ -21,11 +21,12 @@ def login_user(request):
 			if user.is_active:
 				login(request, user)
 				state = "You've logged in!"
+				return render(request, 'index.html')
 			else:
 				state = "Your username and/or password were incorrect."
 		else:
 			state = "Your account is not active, please contact the site admin."
-
+			
 	return render_to_response('auth.html',{'state':state, 'username': username})
 
 #CSRF tokens not enforced in test environment
