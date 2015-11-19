49a0533dd5574005c832c47b0056e02e7e9e5094
Dax <gerts.dax1@gmail.com>
2015-11-19 15:09:31 -0500

    changes to views

--- dbproject/webstore/views.py
+++ dbproject/webstore/views.py
@@ -1,24 +1,32 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import *
 #import necessary models
 from .models import User, Order, Supplier, Contains, Product
 
+@login_required(login_url='/login/')
 def index(request):
 	return HttpResponse("Welcome to [name of webstore here]'s index.")
 
 #CSRF tokens not enfored in test environment
 @csrf_exempt
 def login_user(request):
-	state = "Please enter login information below"
+	logout(request)
+	try:
+		state
+	except NameError:
+		state = "Please enter login information below"
+
 	username = password = ''
-	if request.POST:
+	if request.method == 'POST':
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 
 		user = authenticate(username=username, password=password)
+		
 		if user is not None:
 			if user.is_active:
 				login(request, user)
@@ -34,24 +42,28 @@ def login_user(request):
 #CSRF tokens not enforced in test environment
 @csrf_exempt
 def register_user(request):
-	state = "Please enter registration information below"
+	try:
+		state
+	except NameError:
+		state = "Please enter registration information below"
+	
 	address = username = password = passwordCheck = email = ''
-	if request.POST:
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		passwordCheck = request.POST.get('please verify password')
-		address = request.POST.get('address')
-		email = request.POST.get('email')
+	if request.method == 'POST':
+		print("yoyo")
+		newUsername = request.POST.get('username')
+		newPassword = request.POST.get('password')
+		newPasswordCheck = request.POST.get('re-password')
+		newAddress = request.POST.get('address')
+		newEmail = request.POST.get('email')
 
 		if password != passwordCheck:
 			state = "Your entered password does not match. Please re-enter"
-		# Attempt to add to database, check successful
-		#else:
-		#	success = 
-			#if success: 
-			#	state = "Your account has successfully been created"
-			#else:
-			#	state = "That username is already in use"
-			#	state = "Some of your information is incorrect"
+			return render_to_response('register.html',{'state':state,'username':username})
 
+		# Attempt to add to database, check successful
+		else:
+			newUser = User(username = newUser,password =newPassword, address = newAddress,email = newEmail)
+			newUser.save()
+			return render_to_response('login.html',{'state':state,'username':username})
+	state = "Please enter correct information"
 	return render_to_response('register.html',{'state':state,'username':username})
