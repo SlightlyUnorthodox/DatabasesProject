cb9527512cb4e7f1b27ffdfb7ad59097ef2049c6
Dax <gerts.dax1@gmail.com>
2015-12-04 20:09:56 -0500

    reconfigured admin page to identify relations

--- dbproject/webstore/admin.py
+++ dbproject/webstore/admin.py
@@ -10,8 +10,29 @@ from .models import Supplier
 from .models import Contains
 from .models import Product
 
-admin.site.register(User)
-admin.site.register(Order)
-admin.site.register(Supplier)
-admin.site.register(Contains)
-admin.site.register(Product)
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ['user_id','user_name','user_password','user_email','user_address','user_is_staff']
+	list_editable = ['user_name','user_password','user_email','user_address','user_is_staff']
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ['order_id','order_date','order_paid','orders','contains']
+	list_editable = ['order_date','order_paid','orders','contains']
+
+class SupplierAdmin(admin.ModelAdmin):
+	list_display = ['supplier_id','supplier_name']
+	list_editable = ['supplier_name']
+
+class ContainsAdmin(admin.ModelAdmin):
+	list_display = ['quantity']
+	list_editable = ['quantity']
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['product_id','product_description','product_price','product_active','product_stock_quantity','product_name','supplies']
+	list_editable = ['product_description','product_price','product_active','product_stock_quantity','product_name','supplies']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Contains, ContainsAdmin)
+admin.site.register(Product, ProductAdmin)
